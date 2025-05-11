d50324773d427e73e63d05c4c4a4d128186ad5f2
Eboné <eliggins@protonmail.com>
2025-05-10 20:16:58 -0400

    done

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 async function fetchPokémonInfo(pokéname) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokéname}`);
-    const pokémonData = await response.json();
-    return pokémonData;
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokéname}`);
+
+        if (!response.ok) {
+            throw new Error(`No Pokémon Found!`);
+        }
+
+        const pokémonData = await response.json();
+        return pokémonData;
+    } catch (error) {
+        console.error('Fetch Error:', error.message);
+        document.getElementById("pokéinfo").innerHTML = error.message;
+    }
 }
 
-const button = document.getElementById("btn") 
-document.addEventListener("submit", async (event) => { 
-event.preventDefault()
+const button = document.getElementById("btn")
+document.addEventListener("submit", async (event) => {
+    event.preventDefault()
     const pokéname = document.getElementById("pokéname").value
     const pData = await fetchPokémonInfo(pokéname);
     const pokémonInfoElement = document.getElementById("pokéinfo");
     pokémonInfoElement.innerHTML =
-`   <h2>${pData.name}</h2>
+        `   <h2>${pData.name}</h2>
     <img src="${pData.sprites.front_default}" alt="${pData.name}">
         <h3> Abilities:</h3>
         <ul>
